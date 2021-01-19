f4d34acb1b0b76b220b0912b9eac25193f5ad435
NewbeeAsk <romannosadchuk@gmail.com>
2021-01-19 18:19:07 +0200

    create service model

--- src/main/java/com/autoservice/models/Service.java
+++ src/main/java/com/autoservice/models/Service.java
@@ -1,4 +1,68 @@
 package com.autoservice.models;
 
-public class Service {
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "services")
+public class Service  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer service_id;
+
+    @NotEmpty(message = "Cost cannot be empty")
+    @Column(name = "cost")
+    private Float cost;
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Service(){}
+
+    public Service(Float cost, String name, String description/*, Check check*/) {
+        this.cost = cost;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(Integer service_id) {
+        this.service_id = service_id;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+
